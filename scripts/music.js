33fd663fed5189edbce2d09223d1cc01d4d75d37
Rich <rkim43@uic.edu>
2016-10-23 10:37:47 +0000

    hoawefawe

--- scripts/music.js
+++ scripts/music.js
@@ -8,7 +8,7 @@ function ajaxCall(request, cb) {
         if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
            if (xmlhttp.status == 200) {
                
-             var result = xmlhttp.responseText;
+             var result = JSON.parse(xmlhttp.responseText);
              console.log(result);
              //var test = JSON.parse(xmlhttp.response);
              //var detailedResults = test.query.results;
@@ -31,19 +31,45 @@ function ajaxCall(request, cb) {
 }
 //http://api.wunderground.com/api/06374893a86b9e3f/geolookup/q/94107.json
 
-function grabMusic(){
+// function grabMusic(){
     //var userZipcode = document.getElementById('zipcode').value;
   //  console.log(userZipcode);
     // https://api.spotify.com/v1/search?q="kendrick lamar"&type=playlist"
-    var musicUrl = '//api.spotify.com/v1/search?q="kendrick lamar"&type=playlist';
+    function createPlaylist(){
+    var musicUrl = '//api.spotify.com/v1/search?q="Gambino"&type=playlist';
     ajaxCall(musicUrl, function(result)
     {
         music = result;
-        //console.log('hello');
+        let uri = music.playlists.items[0].uri;
+        console.log(music.playlists.items[0].uri);
+        
+        
+        var ifrm = document.createElement('iframe');
+      //  ifrm.setAttribute('id', 'ifrm'); // assign an id
+        
+        //document.body.appendChild(ifrm); // to place at end of document
+        
+        // to place before another page element
+        //var el = document.getElementById('marker');
+       // el.parentNode.insertBefore(ifrm, el);
+        
+        var frameUrl = '//embed.spotify.com/?uri=' + uri;
+        // assign url
+        ifrm.setAttribute('src', frameUrl);
+        ifrm.setAttribute('width', '100%');
+        ifrm.setAttribute('height', 400);
+        ifrm.setAttribute('frameborder', 0);
+        ifrm.setAttribute('allowtransparency', true);
+        ifrm.setAttribute('class', 'card-shadow')
+        
+        document.getElementById("show-playlist").appendChild(ifrm);
+        console.log('hello');
     });
+    
+    }
     setTimeout(function() { console.log("music test" + music ); }, 3000);
-}
-
+//}
+createPlaylist();
 console.log(music);
 //var zipcode = document.getElementById("zipcode");
 //console.log('hello ' + document.getElementById('zipcode').value);
